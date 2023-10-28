91960e185bb8714f74e2d0049db0d3747f1aef85
Lexie Wang <travelaroundthemoon@gmail.com>
2023-10-28 03:04:50 -0700

    Update website aesthetics

--- age_calculator.py
+++ age_calculator.py
@@ -10,15 +10,29 @@ def index():
         birth_year = int(request.form['birth_year'])
         birth_month = int(request.form['birth_month'])
         birth_date = int(request.form['birth_date'])
-
-        today = datetime.today()
-        birth_datetime = datetime(birth_year, birth_month, birth_date)
-
-        age = today.year - birth_datetime.year - ((today.month, today.day) < (birth_datetime.month, birth_datetime.day))
-
+        age = calculate_age(birth_year, birth_month, birth_date)
         return render_template('index.html', age=age)
     return render_template('index.html', age=None)
 
 
-if __name__ == "__main__":
+def calculate_age(year, month, day):
+    today = datetime.today()
+    birthdate = datetime(year, month, day)
+
+    years = today.year - birthdate.year
+    months = today.month - birthdate.month
+    days = today.day - birthdate.day
+
+    if days < 0:
+        months -= 1
+        days += 30
+
+    if months < 0:
+        years -= 1
+        months += 12
+
+    return f"{years} years, {months} months, {days} days"
+
+
+if __name__ == '__main__':
     app.run(debug=True)
